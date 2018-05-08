ae21b264fe488d366a279a5436982de5dd16a83c
malcodeman <26983729+malcodeman@users.noreply.github.com>
2018-05-09 00:14:55 +0200

    Styled toolbar

--- src/features/homepage/components/Toolbar.js
+++ src/features/homepage/components/Toolbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Wrapper = styled.div`
   flex-basis: 64px;
@@ -8,10 +9,20 @@ const Wrapper = styled.div`
 const Header = styled.header`
   top: 0;
   position: fixed;
-  height: 64px;
   background-color: #fff;
   border-bottom: 1px solid rgba(0, 0, 0, 0.1);
   width: 100%;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 992px;
+  margin: 0 auto;
+  width: 100%;
+  height: 64px;
+  padding: 0 20px;
 `;
 
 class Toolbar extends Component {
@@ -22,7 +33,10 @@ class Toolbar extends Component {
     return (
       <Wrapper>
         <Header>
-          <span onClick={this.signout}>Sign Out</span>
+          <Nav>
+            <Link to="/">Confessio</Link>
+            <span onClick={this.signout}>Sign Out</span>
+          </Nav>
         </Header>
       </Wrapper>
     );
