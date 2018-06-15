5fd03d3e56de0c89d36032ce55f0956a6eb95116
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-06-15 07:05:33 +0200

    Updates post test

--- src/features/posts/__tests__/index.js
+++ src/features/posts/__tests__/index.js
@@ -5,10 +5,15 @@ import { MemoryRouter } from "react-router-dom";
 import Post from "../components/Post";
 import PostLoading from "../components/PostLoading";
 
+const comments = [
+  { id: 1, body: "comment 1", user: { username: "admin" } },
+  { id: 2, body: "comment 2", user: { username: "test" } }
+];
+
 test("Post matches snapshot", () => {
   const component = renderer.create(
     <MemoryRouter>
-      <Post />
+      <Post comments={comments} />
     </MemoryRouter>
   );
   let tree = component.toJSON();
