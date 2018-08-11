c4d6d4a6a4584f3d6977903f81bd6bdafb1f2932
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-08-11 13:42:09 +0200

    Adds loader

--- src/core/styles/themes/dark.js
+++ src/core/styles/themes/dark.js
@@ -1,5 +1,9 @@
 export default {
+  brand: "#ed4956",
   background: "rgba(0, 0, 0, 0.8)",
+  grey: "rgba(25, 25, 25, 1)",
+  black: "#000",
+  white: "#fff",
   primary: "#fff",
   secondary: "#000",
   borderRadius: "3px"
