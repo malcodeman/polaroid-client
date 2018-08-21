1df240c2bd3a3592bd245cf28a978f7a10dd0921
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-08-21 08:56:42 +0200

    Fixes users tests

--- src/features/users/__tests__/index.js
+++ src/features/users/__tests__/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import "jest-styled-components";
 
 import Modal from "../components/Modal";
 import Posts from "../components/Posts";
