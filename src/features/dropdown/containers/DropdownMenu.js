47f0168f6e02108f6335bfc353d1bddaa056b2cd
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-08-28 21:05:14 +0200

    Adds outside click handler for dropdown menu

--- src/features/dropdown/containers/DropdownMenu.js
+++ src/features/dropdown/containers/DropdownMenu.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import { Manager, Popper, Reference } from "react-popper";
+import OutsideClickHandler from "react-outside-click-handler";
+import styled from "styled-components";
 
 import Toolbar from "../../toolbar/containers";
 import DropdownContent from "./DropdownContent";
+
+const ReferenceWrapper = styled.div`
+  height: 36px;
+  display: flex;
+  align-items: center;
+`;
 
 class DropdownMenu extends Component {
   state = {
@@ -13,27 +21,32 @@ class DropdownMenu extends Component {
       openMenu: !prevState.openMenu
     }));
   };
+  handleCloseMenu = () => {
+    this.setState({ openMenu: false });
+  };
   render() {
     const { openMenu } = this.state;
     return (
-      <Manager>
-        <Reference>
-          {({ ref }) => (
-            <div ref={ref} onClick={this.handleToggleMenu}>
-              <Toolbar />
-            </div>
-          )}
-        </Reference>
-        {openMenu ? (
-          <Popper placement="bottom">
-            {({ ref, style, placement }) => (
-              <div ref={ref} style={style} data-placement={placement}>
-                <DropdownContent handleToggleMenu={this.handleToggleMenu} />
-              </div>
+      <OutsideClickHandler onOutsideClick={this.handleCloseMenu}>
+        <Manager>
+          <Reference>
+            {({ ref }) => (
+              <ReferenceWrapper innerRef={ref} onClick={this.handleToggleMenu}>
+                <Toolbar />
+              </ReferenceWrapper>
             )}
-          </Popper>
-        ) : null}
-      </Manager>
+          </Reference>
+          {openMenu ? (
+            <Popper placement="bottom">
+              {({ ref, style, placement }) => (
+                <div ref={ref} style={style} data-placement={placement}>
+                  <DropdownContent handleToggleMenu={this.handleToggleMenu} />
+                </div>
+              )}
+            </Popper>
+          ) : null}
+        </Manager>
+      </OutsideClickHandler>
     );
   }
 }
